58c54f69a489d9effc2ba48f2948a23a7dc7b90d
hitomizavr <hitomizavr@gmail.com>
2021-05-15 01:41:41 +0300

    create: app.js

--- app.js
+++ app.js
@@ -1,1 +1,94 @@
-// Начало
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/not-found-err');
+const { createUser, login } = require('./controllers/user');
+const auth = require('./middlewares/auth');
+const myErrors = require('./middlewares/errors');
+
+// Слушаем 3000 порт
+const { PORT = 3001, DB_PTH = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect(`${DB_PTH}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+// Ограничение количества запросов в единицу времени
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+});
+
+app.use(limiter);
+
+// Секурити заголовки
+app.use(helmet());
+
+// Выносим роуты не требующие авторизации в корневой файл app.js,
+// т.к все остальные роуты находящиеся в routes user / card будут доступны только при auth
+
+app.use(requestLogger);
+
+// Код для тестирования, после надо удалить!!!!
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// Роуты не требующие авторизации
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    password: Joi.string().required().min(8).max(50),
+  }),
+}), login);
+
+// авторизация
+app.use(auth);
+
+// роуты, которым авторизация нужна
+app.use('/', require('./routes/user'));
+app.use('/', require('./routes/movie'));
+
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+// здесь обрабатываем все ошибки
+app.use(errors());
+
+app.use(myErrors);
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
